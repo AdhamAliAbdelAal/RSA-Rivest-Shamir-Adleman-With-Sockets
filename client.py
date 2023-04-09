951d8678e186328f2d063e252ca60fac355c838b
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-04-09 14:35:01 +0200

    feat: send long messages

--- client.py
+++ client.py
@@ -39,21 +39,42 @@ print("the other client public key is: ",public_key)
 # this function handles the client connection
 def receive():
     while True:
-        message=client.recv(1024).decode('utf-8')
-        message=decrypt(int(message),d,n)
-        message=decoder(message)
+        # Receive the encrypted message
+        encrypted_message=client.recv(1024).decode('utf-8')
+
+        # Get the number of sets of 5 characters
+        encrypted_message=encrypted_message.split(' ')
+        message_sets_len=len(encrypted_message)
+        print(encrypted_message)
+        message=''
+        for i in range(message_sets_len):
+            decrypted_block=decrypt(int(encrypted_message[i]),d,n)
+            message+=decoder(decrypted_block)    
+        
         print(f'He:{message}')
 
 def send():
     while True:
+        # Get the message from the user
         message = input()
+
+        # Terminate the connection if the user enter x
         if(message=='x'):
             client.send("DISCONNECT".encode('utf-8'))
             client.close()
             break
-        message=encoder(message)
-        message=str(encrypt(message,public_e,public_n))
-        client.send(message.encode('utf-8'))
+
+        # Append spaces to the message to make it a multiple of 5
+        message_len=len(message)
+        if(message_len%5!=0):
+            message+=' '*(5-message_len%5)
+            message_len+=(5-message_len%5)
+        message_encrypted=[]
+        for i in range(0,message_len,5):
+            message_encoded=encoder(message[i:i+5])
+            message_encrypted.append(str(encrypt(message_encoded,public_e,public_n)))
+        message_encrypted=' '.join(message_encrypted)
+        client.send(message_encrypted.encode('utf-8'))
 
 
 threading.Thread(target=receive).start()
